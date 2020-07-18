5e3063e5a8843e2391595c9eae1c758ec4df4459
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2020-07-18 14:31:51 +0100

    Added filter and modal lightbox functionality to the project.

--- js/app.js
+++ js/app.js
@@ -1,94 +1,166 @@
-// show cart
+// filterButton start
 
-(function(){
-    //target cart button
-    const cartInfo = document.getElementById('cart-info');
-    const cart = document.getElementById('cart');
+(function () {
+    // vscode commit test
 
-    cartInfo.addEventListener('click', function(){
-        cart.classList.toggle('show-cart');
-    })
+    const el = document.getElementsByClassName("store-item");
+    const inputBar = document.getElementById("search-item");
+    const filterButtons = document.getElementsByClassName("filter-btn");
 
-
-})();
-
-// add items to the cart
-
-(function(){
-
-const cartBtn = document.querySelectorAll('.store-item-icon');
-
-cartBtn.forEach(function(btn){
-    btn.addEventListener('click', function(event){
-        //make sure event fires only if it has a parent of a certain class.
-        if(event.target.parentElement.classList.contains('store-item-icon')){
-
-            let fullPath = event.target.parentElement.previousElementSibling.src;
-
-            let pos = fullPath.indexOf('img') + 3; //use the 3 to get rid of the 'img' string
-
-            let partPath = fullPath.slice(pos);
-
-            const item = {};
-
-            item.img = `img-cart${partPath}`;
-
-            let name = event.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
-
-            item.name = name;
-
-            let price = event.target.parentElement.parentElement.nextElementSibling.children[0].children[1].textContent;
-
-            let finalPrice = price.slice(1).trim();
-
-            item.price = finalPrice;
-
-            const cartItem = document.createElement('div');
-
-            cartItem.classList.add('cart-item', 'd-flix', 'justify-content-between', 'text-capitalize', 'my-3');
-
-            cartItem.innerHTML = `<div class="cart-item d-flex justify-content-between text-capitalize my-3"><img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
-              <div class="item-text"><p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p><span>$</span>
-                <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span></div><a href="#" id='cart-item-remove' class="cart-item-remove"><i class="fas fa-trash"></i></a></div>`;
-
-        //select cart
-
-        const cart = document.getElementById('cart');
-        const total = document.querySelector('.cart-total-container');
-
-        cart.insertBefore(cartItem, total);
-        alert('item added to the cart');
-
-        showTotals();
-
+    const setBlock = (event) => {
+        if (event) {
+            event.preventDefault();
         }
-    });
-});
+        for (let i = 0; i < 12; i++) {
+            el[i].style.display = "block";
+        }
+    };
 
-// show totals
-function showTotals(){
 
-    const total = [];
-    const items = document.querySelectorAll('.cart-item-price');
-    items.forEach(function(item){
-        total.push(parseFloat(item.textContent));
-    })
-    
-    const totalMoney = total.reduce(function(total, item){
-        total += item;
-        return total;
-    },0);
+    const filtering = (inputValue) => {
+        setBlock();
+        for (let i = 0; i < 12; i++) {
+            let storeItem = el[i].dataset.item;
+            if ((!storeItem.includes(inputValue))) {
+                el[i].style.display = "none";
+            }
+        }
+    };
 
-    const finalMoney = totalMoney.toFixed(2);
-    
-    document.getElementById('cart-total').textContent = finalMoney;
-    document.querySelector('.item-total').textContent = finalMoney;
-    document.getElementById('item-count').textContent = total.length;
-}
+    const displayValue = (event) => {
+        if (event.keyCode === 13) {
+            setBlock();
+            let myInput = inputBar.value.toLowerCase();
+            event.preventDefault();
+            filtering(myInput);
+        }
+
+    };
+
+    const makeHandler = (param) => {
+        return function (event) {
+            event.preventDefault();
+            filtering(param);
+        };
+    };
+
+
+
+    for (let i = 1; i < filterButtons.length; i++) {
+        // gets all the items i.e cakes, doughnuts... and sets them to handlerParam per loop
+        let handlerParam = filterButtons[i].dataset.filter;
+        filterButtons[i].addEventListener("click", makeHandler(handlerParam));
+    };
+
+    filterButtons[0].addEventListener("click", setBlock);
+    inputBar.addEventListener("keyup", displayValue);
 
 })();
 
-//Things learned
-//DOM traversal using previousElementSibling
-//element.insertBefore
-//reduct method
+// filterButton end
+
+// Get all the images on the page and add them to an array.
+const pics = document.getElementsByClassName("card-img-top");
+let imageList = [];
+for (let i = 0; i < pics.length; i++) {
+    const itemPicture = pics[i].src;
+    imageList.push(itemPicture);
+};
+
+
+// for (let i = 0; i < pics.length; i++) {
+//   pics[i].onclick = () => {
+//     const toShow = document.querySelector(".lightbox-container");
+//     const showItem = document.querySelector(".lightbox-item");
+//     const closeButton = document.querySelector(".lightbox-close");
+//     const previousButton = document.querySelector(".btnLeft")
+//     const nextButton = document.querySelector(".btnRight")
+//     toShow.classList.add("show");
+
+//     let currentItem = pics[i]; // get the img element of the current iteration
+//     // console.log(currentItem)
+//     //gets the index of the currrent background image from the imageList array.
+//     let currentIndex = imageList.indexOf(currentItem.src);
+//     showItem.style.backgroundImage = `url(${currentItem.src})`;
+
+//     const previousImage = () => {
+//       if (currentIndex > 0) {
+//         currentIndex--;
+//       } else {
+//         currentIndex = 0;
+//       }
+//       let imageInImageList = imageList[currentIndex];
+//       console.log(imageInImageList);
+//       showItem.style.backgroundImage = `url(${imageInImageList})`
+//     };
+
+//     const nextImage = () => {
+//       if (currentIndex < imageList.length - 1) {
+//         currentIndex++;
+//       } else {
+//         currentIndex = imageList.length - 1;
+//       }
+//       let imageInImageList = imageList[currentIndex];
+//       console.log(imageInImageList);
+//       showItem.style.backgroundImage = `url(${imageInImageList})`
+//     };
+
+//     const closeModal = () => {
+//       toShow.classList.remove("show");
+//     };
+
+//     previousButton.addEventListener("click", previousImage);
+//     nextButton.addEventListener("click", nextImage);
+//     closeButton.addEventListener("click", closeModal);
+//   }
+// }
+
+(function () {
+    const myModal = (event) => {
+        const toShow = document.querySelector(".lightbox-container");
+        const showItem = document.querySelector(".lightbox-item");
+        const closeButton = document.querySelector(".lightbox-close");
+        const previousButton = document.querySelector(".btnLeft")
+        const nextButton = document.querySelector(".btnRight")
+        toShow.classList.add("show");
+
+        let currentItem = event.target;// get the img element of the current iteration
+        //gets the index of the currrent background image from the imageList array.
+        let currentIndex = imageList.indexOf(currentItem.src);
+        showItem.style.backgroundImage = `url(${currentItem.src})`;
+
+
+        const previousImage = () => {
+            if (currentIndex > 0) {
+                currentIndex--;
+            } else {
+                currentIndex = 0;
+            }
+            let imageInImageList = imageList[currentIndex];
+            showItem.style.backgroundImage = `url(${imageInImageList})`
+        };
+
+        const nextImage = () => {
+            if (currentIndex < imageList.length - 1) {
+                currentIndex++;
+            } else {
+                currentIndex = imageList.length - 1;
+            }
+            let imageInImageList = imageList[currentIndex];
+            showItem.style.backgroundImage = `url(${imageInImageList})`
+        };
+
+        const closeModal = () => {
+            toShow.classList.remove("show");
+        };
+
+        previousButton.addEventListener("click", previousImage);
+        nextButton.addEventListener("click", nextImage);
+        closeButton.addEventListener("click", closeModal);
+
+    };
+
+    for (let i = 0; i < pics.length; i++) {
+        pics[i].addEventListener("click", myModal);
+    };
+})();
